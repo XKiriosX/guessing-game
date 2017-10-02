25294c9cec5c63cb218c51e57eff12b1ff4946d0
Daniel <slayerdog4@gmail.com>
2017-10-02 19:12:26 +0300

    Add files via upload

---
+++ src/guessing-game.js
@@ -0,0 +1,22 @@
+class GuessingGame {
+    constructor() {}
+
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    guess() {
+        return Math.round((this.max + this.min) / 2);
+    }
+
+    lower() {
+        this.max = Math.round((this.max + this.min) / 2);
+    }
+
+    greater() {
+        this.min = Math.round((this.max + this.min) / 2);
+    }
+}
+
+module.exports = GuessingGame;
